c91e2a46bc607f9af744e852f77f93baf3aa7873
Pope24 <110238804+Pope24@users.noreply.github.com>
2023-05-25 13:45:56 +0700

    authentication in login feature

--- dn-cinema/src/main/java/com/example/dncinema/repository/ITicketRepository.java
+++ dn-cinema/src/main/java/com/example/dncinema/repository/ITicketRepository.java
@@ -4,8 +4,10 @@ import com.example.dncinema.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ITicketRepository extends JpaRepository<Ticket, Integer> {
     @Query(value = "select * from ticket where id_ticket=:id", nativeQuery = true)
-    Ticket findTicketById(@Param("idTicket") Integer id);
+    Ticket findTicketById(@Param("id") Integer id);
 }
