62e28c4489055852ba3b19336bf6e5345bfa567d
tyem123ks2 <thanhtuan02051011@gmail.com>
2023-05-25 10:46:40 +0700

    update develop/discount

---
+++ dn-cinema/src/main/java/com/example/dncinema/service/discount/impl/DiscountService.java
@@ -0,0 +1,8 @@
+package com.example.dncinema.service.discount.impl;
+
+import com.example.dncinema.service.discount.IDiscountService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DiscountService implements IDiscountService {
+}
