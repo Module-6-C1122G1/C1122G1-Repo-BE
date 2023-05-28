de431bac2f20f809131463c20617a08c221d9311
tyem123ks2 <thanhtuan02051011@gmail.com>
2023-05-28 14:38:41 +0700

    TuanLT - Update JUnit5 - Test Discount List and Delete Discount

--- dn-cinema/src/main/java/com/example/dncinema/controller/TicketController.java
+++ dn-cinema/src/main/java/com/example/dncinema/controller/TicketController.java
@@ -1,30 +1,30 @@
-//package com.example.dncinema.controller;
-//
-//import com.example.dncinema.model.Ticket;
-//import com.example.dncinema.service.ticket.ITicketService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/ticket")
-//@CrossOrigin("*")
-//public class TicketController {
-//    @Autowired
-//    ITicketService iTicketService;
-//
-//    /**
-//     * Create: QuynhHTN
-//     * Date create: 24/05/2023
-//     * @param id
-//     * @return
-//     */
-//    @GetMapping("/detail/{id}")
-//    public ResponseEntity<?> findTicketById(@PathVariable Integer id) {
-//        Ticket ticket=iTicketService.findTicketById(id);
-//        if(ticket==null){
-//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//        }return new ResponseEntity<>(ticket,HttpStatus.OK);
-//    }
-//}
+package com.example.dncinema.controller;
+
+import com.example.dncinema.model.Ticket;
+import com.example.dncinema.service.ticket.ITicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ticket")
+@CrossOrigin("*")
+public class TicketController {
+    @Autowired
+    ITicketService iTicketService;
+
+    /**
+     * Create: QuynhHTN
+     * Date create: 24/05/2023
+     * @param id
+     * @return
+     */
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<?> findTicketById(@PathVariable Integer id) {
+        Ticket ticket=iTicketService.findTicketById(id);
+        if(ticket==null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }return new ResponseEntity<>(ticket,HttpStatus.OK);
+    }
+}
