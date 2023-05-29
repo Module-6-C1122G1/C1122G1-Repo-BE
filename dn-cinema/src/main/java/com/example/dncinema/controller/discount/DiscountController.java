688f4ed228b251db151e77daf2b8a75f1d06217c
tyem123ks2 <thanhtuan02051011@gmail.com>
2023-05-29 14:05:03 +0700

    TuanLT - Fix DiscountController, RequestMapping("/api/discount") - Fix JavaDOC

--- dn-cinema/src/main/java/com/example/dncinema/controller/discount/DiscountController.java
+++ dn-cinema/src/main/java/com/example/dncinema/controller/discount/DiscountController.java
@@ -28,7 +28,6 @@ public class DiscountController {
     /**
      * Create: TuanLT.
      * Date create: 24/05/2023.
-     *
      * @param name     "Dùng để tìm kiếm khuyến mãi theo tên".
      * @param pageable "Dùng để phân trang".
      * @return "Trả về Page hiển thị danh sách khuyến mãi, nếu người dùng tiến hành tìm kiếm thì Page này sẽ hiển thị danh sách được tìm kiếm theo tên".
@@ -39,26 +38,6 @@ public class DiscountController {
     public Page<DiscountDTO> showList(@RequestParam(required = false, defaultValue = "") String name,
                                       @PageableDefault(direction = Sort.Direction.DESC, size = 5) Pageable pageable) {
         return discountService.findByName(name, pageable);
-    }
-
-    /**
-     * Create: TuanLT
-     * Date create: 24/05/2023.
-     *
-     * @param id "Tìm kiếm id của 1 khuyến mãi và xóa nó".
-     */
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable("id") int id) {
-        if (!discountRepository.existsById(id)) {
-            return new ResponseEntity<>("ID không tồn tại!", HttpStatus.NOT_FOUND);
-        }
-        discountService.delete(id);
-        return new ResponseEntity<>("Xóa thành công!", HttpStatus.OK);
-//    @DeleteMapping("")
-//    @ResponseStatus(HttpStatus.OK)
-//    public void delete(@RequestParam(required = false) Long id) {
-//        discountService.delete(id);
     }
 
     /**
@@ -125,5 +104,20 @@ public class DiscountController {
         discountService.updateDiscount(discountDTO.getIdDiscount(),discountDTO.getNameDiscount(), discountDTO.getDateStart(),
                 discountDTO.getDateEnd(), discountDTO.getImageDiscount(),discountDTO.getDescribeDiscount(), discountDTO.getPercentDiscount());
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    /**
+     * Create: TuanLT
+     * Date create: 24/05/2023.
+     * @param id "Tìm kiếm id của 1 khuyến mãi và xóa nó".
+     */
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") int id) {
+        if (!discountRepository.existsById(id)) {
+            return new ResponseEntity<>("ID không tồn tại!", HttpStatus.NOT_FOUND);
+        }
+        discountService.delete(id);
+        return new ResponseEntity<>("Xóa thành công!", HttpStatus.OK);
     }
 }
