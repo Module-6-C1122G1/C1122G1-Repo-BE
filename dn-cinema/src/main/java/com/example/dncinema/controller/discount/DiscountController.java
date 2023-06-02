701bce626e7be91c1bb3751fe4b57fe856d02ca1
tyem123ks2 <thanhtuan02051011@gmail.com>
2023-06-02 10:13:01 +0700

    TuanLT - fix api/employee/discount

--- dn-cinema/src/main/java/com/example/dncinema/controller/discount/DiscountController.java
+++ dn-cinema/src/main/java/com/example/dncinema/controller/discount/DiscountController.java
@@ -17,7 +17,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/public/discount")
+@RequestMapping("/api/employee/discount")
 @CrossOrigin("*")
 public class DiscountController {
     @Autowired
@@ -129,14 +129,5 @@ public class DiscountController {
         }
         return new ResponseEntity<>(listDiscount, HttpStatus.OK);
     }
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Discount> findByIdDiscount(@PathVariable Integer id) {
-//        Discount detailDiscount = discountService.findById(id);
-//        if (detailDiscount == null) {
-//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//        }
-//        return new ResponseEntity<>(detailDiscount, HttpStatus.OK);
-//    }
-
 
 }
