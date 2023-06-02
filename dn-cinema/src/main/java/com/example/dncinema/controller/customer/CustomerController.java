b95232ac56e93223d1e1143188153169a2532966
Ddloyd <quocanh25022001@gmail.com>
2023-06-02 15:19:04 +0700

    AnhNQ update code

--- dn-cinema/src/main/java/com/example/dncinema/controller/customer/CustomerController.java
+++ dn-cinema/src/main/java/com/example/dncinema/controller/customer/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/customer")
+@RequestMapping("/api/employee/customer")
 public class CustomerController {
 
     @Autowired
